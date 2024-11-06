9abca38579ff4af6ed6d2485ece4367b95cd19e9
Yebin Lee <139226103+shail1027@users.noreply.github.com>
2024-11-06 14:19:05 +0900

    [Bronze IV] Title: $A + B - C$, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/31403. $A ＋ B － C$/$A ＋ B － C$.py
@@ -0,0 +1,13 @@
+intList = list()
+strList = list()
+
+for _ in range(3) :
+    i = input()
+    intList.append(int(i))
+    strList.append(i)
+
+print(intList[0]+intList[1]-intList[2])
+print(int(strList[0]+strList[1])-int(strList[2]))
+
+    
+    
