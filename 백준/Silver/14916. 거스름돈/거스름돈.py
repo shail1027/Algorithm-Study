3a4d0288f98dc0ad2edc2bc6d10426bd19300a94
Yebin Lee <139226103+shail1027@users.noreply.github.com>
2025-03-20 16:01:08 +0900

    [Silver V] Title: 거스름돈, Time: 92 ms, Memory: 108384 KB -BaekjoonHub

---
+++ 백준/Silver/14916. 거스름돈/거스름돈.py
@@ -0,0 +1,21 @@
+import sys
+
+n = int(sys.stdin.readline().rstrip())
+cnt = 0
+
+while True:
+    if n % 5 == 0:
+        cnt += n // 5
+        break
+
+    else:
+        n -= 2
+        cnt += 1
+
+    if n < 0:
+        break
+
+if n < 0:
+    print(-1)
+else:
+    print(cnt)
