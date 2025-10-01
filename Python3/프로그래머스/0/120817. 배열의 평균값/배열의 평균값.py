69e3418bcb187c8cbed173934cc374a2f5bb6be0
Yebin Lee <139226103+shail1027@users.noreply.github.com>
2025-10-01 21:28:52 +0900

    Delete Python3/프로그래머스 directory

--- Python3/프로그래머스/0/120817. 배열의 평균값/배열의 평균값.py
+++
@@ -1,6 +0,0 @@
-def solution(numbers):
-    answer = 0
-    for i in numbers :
-        answer += i
-    answer /= len(numbers)
-    return answer
--- Python3/프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.py
+++
@@ -1,9 +0,0 @@
-def solution(phone_number):
-    answer = ''
-
-    for i in range(len(phone_number)):
-        if i < len(phone_number) - 4:
-            answer += '*'
-        else:
-            answer += phone_number[i]
-    return answer
